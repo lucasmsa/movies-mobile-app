f2148ec5484ae87ed3a41654a8b50ea4b7e40a50
Lucas <lucasmsea@outlook.com>
2021-12-20 09:30:54 -0300

    feat: start react-native application

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+
+const App: React.FC = () => (
+  <SafeAreaView>
+    <Text>Supper</Text>
+  </SafeAreaView>
+)
+
+export default App;
